51c9795a920593028024d4d440d488e5235dc9de
Rayan Aouf <rayanaouf1512@gmail.com>
2024-09-05 22:05:22 +0100

    pos page html update

---
+++ pos_ar/pos_ar/page/pos/pos.js
@@ -0,0 +1,11 @@
+frappe.pages['pos'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'POS',
+		single_column: true
+	});
+
+	$(frappe.render_template("pos" , {} )).appendTo(page.body);
+
+
+}
